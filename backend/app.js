aea8a8c564d5075ec0fc3084386cb749c0c8de23
Jaime <jaime.romo.guti@gmail.com>
2020-01-29 12:17:38 +0100

    api preparada para empezar a trabajar con las rutas

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,11 @@ const PORT = 4000;
 const db = require('./config/bbdd');
 const Usuario = require('./modelos/Usuario');
 
+app.use(bodyParser.json());
+app.use(cors());
+
+const rutasAPI = express.Router();
+//http://127.0.0.1:4000/api/usuarios
+app.use('/api/usuarios', rutasAPI);
 
 app.listen(PORT, () => console.log(`Servidor activo ${PORT}`));
