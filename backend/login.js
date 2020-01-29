67db2b0c8a5bbed3ab9114463aae77961e9f1198
Jaime <jaime.romo.guti@gmail.com>
2020-01-29 14:44:08 +0100

    esperemos que la buena

---
+++ backend/login.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 4000;
+const db = require('./config/bbdd');
+const Usuario = require('./modelos/Usuario');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const rutasAPI = express.Router();
+//http://127.0.0.1:4000/api/usuarios
+app.use('/api/usuarios', rutasAPI);
+
+rutasAPI.route('/').get((req,res) => {
+   console.log("API FUNCIONANDO!");
+});
+
+//CREAR USUARIO JAIME
+// rutasAPI.route('/registro').post((req, res) => {
+//     let nuevoUsuario = new Usuario(req.body);
+
+//     nuevoUsuario.save().then((usuario) => {
+//         console.log('Registrado');
+//         res.status(200).json({
+//             'usuario': 'Creado satisfactoriamente'
+//         })
+//     }).catch((err) => {
+//         console.log('Falloooo');
+//         res.status(400).send('El registro ha fallado');
+//     });
+//     console.log('La request ha sido procesada');
+// })
+
+rutasAPI.route('/login').post((req, res) => {
+    let correo = req.body.email;
+    let password = req.body.password;
+
+    Usuario.findOne({email: correo}, (err, user) => {
+        console.log(user);
+        if(err){
+            res.json(err);
+        } else {
+            res.json(user);
+        }
+    });
+
+    
+})
+
+
+app.listen(PORT, () => console.log(`Servidor activo ${PORT}`));
