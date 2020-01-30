afe288d111aead8ed2ae66f86925ae97674c0a53
Pako Roncero <pakitovk.29@gmail.com>
2020-01-30 17:14:59 +0100

    Componte registro y base enrutado

--- frontend/applergic/src/app/app-routing.module.ts
+++ frontend/applergic/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearUsuarioComponent } from './componentes/crear-usuario/crear-usuario.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "registro",
+    component: CrearUsuarioComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/applergic/src/app/app.module.ts
+++ frontend/applergic/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import { RouterModule, Routes } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { CrearUsuarioComponent } from './componentes/crear-usuario/crear-usuario.component';
+import { CrearEmergenciaComponent } from './componentes/crear-emergencia/crear-emergencia.component';
 import { IntroduccionComponent } from './componentes/introduccion/introduccion.component';
 
 
@@ -12,13 +16,52 @@ import { IntroduccionComponent } from './componentes/introduccion/introduccion.c
   declarations: [
     AppComponent,
     LoginComponent,
-    IntroduccionComponent
+    IntroduccionComponent,
+    AppComponent,
+    CrearUsuarioComponent,
+    CrearEmergenciaComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule
+    ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/*
+const rutasApp: Routes = [
+
+  {
+    path: "lista",
+    component: ListaUsuariosComponent
+  },
+  {
+    path: "menu",
+    component: MenuComponent
+  },
+  {
+    path: "",
+    redirectTo: "/lista",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: ErrorComponent
+  }
+
+]
+
+//@NgModule dice a Angular que esta clase es un  modulo
+@NgModule({
+  declarations:[
+    AplicacionComponent, //los componentes hay que declararlos
+    NuevoUsuarioComponent, ListaUsuariosComponent, EditarUsuarioComponent, MenuComponent, ErrorComponent,
+    //stylesUrls:
+  ],
+  imports:[BrowserModule, FormsModule, RouterModule.forRoot(rutasApp,{enableTracing: true})], //Aqui solo se importan otros modulos,
+  bootstrap:[AplicacionComponent]
+*/
