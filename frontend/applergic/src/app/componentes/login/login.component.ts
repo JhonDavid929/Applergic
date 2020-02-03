5f3e0adbbb1bb14861a8049d9f77200b6f268a29
Jaime <jaime.romo.guti@gmail.com>
2020-02-03 11:25:19 +0100

    formulario log in

--- frontend/applergic/src/app/componentes/login/login.component.ts
+++ frontend/applergic/src/app/componentes/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioServicio } from '../../servicios/usuarioServicio';
+import { MensajeServicio } from '../../servicios/mensajeServicio';
+import { Usuario } from 'src/app/entidades/usuario';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-    
-  constructor() { }
+
+  public usuarioLogin;
+
+  constructor(private usuarioServi: UsuarioServicio, private mensajeServi: MensajeServicio) { 
+    this.usuarioLogin = {
+      "email": "",
+      "password": "",
+      }
+  }
 
   ngOnInit() {
   }
 
+  logIn(form){
+    this.usuarioServi.logIn(this.usuarioLogin);
+    console.log(this.usuarioLogin);
+  }
 }
